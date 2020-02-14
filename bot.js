73f1aa90accc4a26f9f9147d45b628e77c92cfdd
Mseza <l_gottar@orange.fr>
2020-02-15 00:05:02 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,57 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+let guildBot = "";
+let newRole = "";
+let roleRules = "";
+let modoRole = "";
+var membersRole = new Array();
+
+client.on("ready", () => {
+
+  console.log("I am ready!");
+
+});
+
+client.on("message", message => {
+
+  guildBot = message.member.guild;
+  newRole = guildBot.roles.find("name", "Touriste");
+  modoRole = guildBot.roles.find("name", "Staff");
+
+  if(message.author.bot) return;
+
+  if(message.member.roles.has("235870151231143936")) {
+    
+    if(message.content === "!clear"){
+    
+      message.channel.bulkDelete(99);
+      message.channel.send("Lisez les règles !");
+      
+    } else {
+  
+    return;
+      
+    }
+
+  }
+    
+  if(message.channel.id === "676820473480216577"){
+
+    if(message.content === "J'ai lu et j'approuve le règlement"){
+
+      message.channel.bulkDelete(99);
+      message.member.addRole(newRole);
+      message.channel.send('Lisez les règles !');
+
+    } else {
+
+      message.channel.send("Veuillez relire les règles s'il vous plaît!");
+
+    }
+
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
